1bad1e2bb2b04a086eae3c8b39c381e0092e433b
Thomas Baag <thomas@b2ag.de>
2018-08-21 03:07:18 +0200

    fixed typo

--- per-app-crypted-home.bash
+++ per-app-crypted-home.bash
@@ -30,7 +30,7 @@ HOMECONTAINER="$HOME/crypted-home-for-$APPLICATION_ID"
 SIZE_IN_MiB=1024 # MiB
 FS_TYPE=ext4
 TEAR_DOWN_TIMEOUT=10 # Seconds
-QUITE="$QUITE"
+QUIET="$QUIET"
 
 print_usage() {
   echo "Usage: $0 executable [arguments...]" > /dev/stderr
@@ -42,12 +42,12 @@ if [ ! -x "$APPLICATION" ]; then
   exit 1
 fi
 
-print_if_not_quite() { [ -z "$QUITE" ] && "$CAT" || true; }
+echo_if_not_quiet() { [ -z "$QUIET" ] && echo "$@" || true; }
 
 escalate_priviledges() {
   if [ "$( "$ID" -u )" != "0" ]; then
-    echo "Need to escalate priviledges..." | print_if_not_quite
-    exec "$SUDO" -E "USER=$USER" "HOME=$HOME" "NEED_FORMAT=$NEED_FORMAT" "QUITE=$QUITE" "$0" "$@"
+    echo_if_not_quiet "Need to escalate priviledges..."
+    exec "$SUDO" -E "USER=$USER" "HOME=$HOME" "NEED_FORMAT=$NEED_FORMAT" "QUIET=$QUIET" "$0" "$@"
   fi
   echo "Something is wrong" > /dev/stderr
   exit 8
@@ -65,7 +65,7 @@ die() {
 # define tear down operation
 tear_down() {
   TEAR_DOWN_TIMEOUT_DATE=$(( $("$DATE" +%s) + TEAR_DOWN_TIMEOUT ))
-  echo "Closing container..." | print_if_not_quite
+  echo_if_not_quiet "Closing container..."
   while "$CRYPTSETUP" status "$APPLICATION_ID" > /dev/null 2>&1; do
     "$CRYPTSETUP" close "$APPLICATION_ID"
     if [ "$( "$DATE" +%s )" -ge "$TEAR_DOWN_TIMEOUT_DATE" ]; then
@@ -80,7 +80,7 @@ tear_down() {
 
 # prepare (1/4) (create empty file if needed)
 if [ "$( "$ID" -u )" != "0" ] && [ ! -e "$HOMECONTAINER" ]; then
-  echo "Create container of size $SIZE_IN_MiB mebibyte..." | print_if_not_quite
+  echo_if_not_quiet "Create container of size $SIZE_IN_MiB mebibyte..."
   NEED_FORMAT=true
   "$DD" bs=$((1024*1024)) count=$SIZE_IN_MiB if=/dev/zero "of=$HOMECONTAINER" || die "Couldn't prepare empty container at \"$HOMECONTAINER\""
   escalate_priviledges "$@"
@@ -105,7 +105,7 @@ fi
 
 # prepare (2/4) (LUKS format)
 if $NEED_FORMAT; then
-  echo "LUKS format container..." | print_if_not_quite
+  echo_if_not_quiet "LUKS format container..."
   "$CRYPTSETUP" -q luksFormat --type luks2 "$HOMECONTAINER" || die "Couldn't LUKS format container at \"$HOMECONTAINER\""
 elif ! "$FILE" "$HOMECONTAINER" | grep -q "LUKS encrypted file"; then
   die "Container missing LUKS header at \"$HOMECONTAINER\""
@@ -115,7 +115,7 @@ fi
 if [ ! -e "$HOMECONTAINER" ]; then
   die "Couldn't find container at \"$HOMECONTAINER\""
 else
-  echo "LUKS open container..." | print_if_not_quite
+  echo_if_not_quiet "LUKS open container..."
   "$CRYPTSETUP" open --type luks "$HOMECONTAINER" "$APPLICATION_ID" || die "Couldn't open container \"$HOMECONTAINER\""
 fi
 
@@ -126,7 +126,7 @@ fi
 
 # prepare (4/4) (mkfs if needed)
 if $NEED_FORMAT; then
-  echo "Format uncrypted container..." | print_if_not_quite
+  echo_if_not_quiet "Format uncrypted container..."
   "$MKFS.$FS_TYPE" "$DMCRYPTED_HOMECONTAINER"
 fi
 
@@ -134,7 +134,7 @@ fi
 trap tear_down SIGTERM SIGINT
 
 # change into new environment
-echo "Change into container..." | print_if_not_quite
+echo_if_not_quiet "Change into container..."
 MOUNT_CMD="\"$MOUNT\" \"$DMCRYPTED_HOMECONTAINER\" \"$HOME\""
 if $NEED_FORMAT; then
   MOUNT_CMD="$MOUNT_CMD && chown \"$USER:\" -R \"$HOME\""
@@ -149,4 +149,4 @@ else
 fi
 
 tear_down
-echo "Everything went fine. Bye!" | print_if_not_quite
+echo_if_not_quiet "Everything went fine. Bye!"
