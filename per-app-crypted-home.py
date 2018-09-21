40c3615a84a5a8179cb323e1c22a4d1f2eb0b9c1
Thomas Baag <thomas@b2ag.de>
2018-09-21 09:42:11 +0200

    more secure version of last fix

--- per-app-crypted-home.py
+++ per-app-crypted-home.py
@@ -362,6 +362,7 @@ def decide_about_resize( config ):
 
 def main():
   logging.basicConfig(format='[{}|%(levelname)s] %(message)s'.format(sys.argv[0]),level=logging.INFO)
+  os.umask(0o77)
   config=parse_arguments()
   logging.debug('Configuration:\n{}'.format(config))
   if not config['superuser']:
@@ -371,7 +372,6 @@ def main():
       config['do_mkfs'] = True
       create_empty_container( config )
     escalate_priviledges( config ) # -> sys.exit
-  os.umask(0o22) # ensure we use a normal umask
   if not os.path.exists( config['container'] ):
     die("Container \"{container}\" does not exist".format(**config))
   if config['do_mkfs']:
@@ -533,7 +533,7 @@ def main():
       if not os.path.exists( config['xdg_runtime_dir'] ):
         if not config['hide_run']:
           die('Refusing to create XDG_RUNTIME_DIR without hiding "/run"')
-        os.makedirs( config['xdg_runtime_dir'], mode=0o755 )
+        os.umask(0o22); os.makedirs( config['xdg_runtime_dir'], mode=0o755 ); os.umask(0o77)
         os.chown( config['xdg_runtime_dir'], config['uid'], config['gid'] )
         os.chmod( config['xdg_runtime_dir'], 0o700 )
       os.environ['XDG_RUNTIME_DIR'] = config['xdg_runtime_dir']
@@ -549,7 +549,7 @@ def main():
     # bind dirs feature
     if 'CLONE_NEWNS' in config['unshare_flags']:
       for bind_dir in config['bind_dirs']:
-        os.makedirs('{}/{}'.format(private_space,bind_dir))
+        os.umask(0o22); os.makedirs('{}/{}'.format(private_space,bind_dir)); os.umask(0o77)
         if libc.mount( 
            '{}/{}'.format(config['home'],bind_dir).encode(), 
            '{}/{}'.format(private_space,bind_dir).encode(), 
